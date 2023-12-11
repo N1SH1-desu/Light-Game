363ced627a977f99b4c10170c9e35790e47f7087
洸太朗 西 <nishinn77@gmail.com>
2023-12-11 15:47:53 +0900

    プロジェクト ファイルを追加します。

---
+++ SourceCode/all.h
@@ -0,0 +1,25 @@
+#pragma once
+
+//******************************************************************************
+//
+//
+//      all.h
+//
+//
+//******************************************************************************
+
+//------< インクルード >---------------------------------------------------------
+#include "../GameLib/game_lib.h"
+#include "../GameLib/input_manager.h"
+#include "../GameLib/obj2d_data.h"
+#include "../GameLib/depth_stencil.h"
+
+#include "work.h"
+#include "user.h"
+#include "common.h"
+
+#include "scene.h"
+#include "game.h"
+#include "title.h"
+
+//******************************************************************************
---
+++ SourceCode/game.cpp
@@ -0,0 +1,108 @@
+//******************************************************************************
+//
+//
+//      ゲーム
+//
+//
+//******************************************************************************
+
+//------< インクルード >---------------------------------------------------------
+#include "all.h"
+
+//------< using >---------------------------------------------------------------
+using namespace GameLib;
+
+//------< 変数 >----------------------------------------------------------------
+Game Game::instance_;
+
+Game::Game() :
+    isPaused(false)
+{
+}
+
+//--------------------------------
+//  初期化処理
+//--------------------------------
+void Game::init()
+{
+    Scene::init();	    // 基底クラスのinitを呼ぶ
+
+    isPaused = false;   // ポーズフラグの初期化
+}
+
+//--------------------------------
+//  終了処理
+//--------------------------------
+void Game::deinit()
+{
+    // テクスチャの解放
+    texture::releaseAll();
+
+    // 音楽のクリア
+    music::clear();
+}
+
+//--------------------------------
+//  更新処理
+//--------------------------------
+void Game::update()
+{
+    using namespace input;
+
+    // ソフトリセット
+    if ((STATE(0) & PAD_SELECT) &&  // 0コンのセレクトボタンが押されている状態で
+        (TRG(0) & PAD_START))       // 0コンのスタートボタンが押された瞬間
+    {
+        changeScene(Title::instance());   // タイトルシーンに切り替える
+        return;
+    }
+
+    // デバッグ文字列表示
+    debug::setString("state:%d", state);
+    debug::setString("timer:%d", timer);
+
+    // ポーズ処理
+    if (input::TRG(0) & PAD_START)
+        isPaused = !isPaused;       // 0コンのスタートボタンが押されたらポーズ状態が反転
+    if (isPaused) return;           // この時点でポーズ中ならリターン
+
+    switch (state)
+    {
+    case 0:
+        //////// 初期設定 ////////
+
+        timer = 0;
+        GameLib::setBlendMode(Blender::BS_ALPHA);   // 通常のアルファ処理
+        
+        state++;    // 初期化処理の終了
+
+        /*fallthrough*/     // case 1:の処理も同時に行う必要があるため、わざとbreak;を記述していない
+
+    case 1:
+        //////// 通常時の処理 ////////
+
+        timer++;
+
+        debug::setString("");
+        debug::setString("Left Up Right Down");
+        debug::setString("Z:Horizontal");
+        debug::setString("X:Vertical");
+
+        break;
+    }
+}
+
+//--------------------------------
+//  描画処理
+//--------------------------------
+void Game::draw()
+{
+    // 画面クリア
+    GameLib::clear(VECTOR4(0, 0, 0, 1));
+
+    // ステンシルモード：通常
+    DepthStencil::instance().set(DepthStencil::MODE::NONE);
+
+}
+
+//******************************************************************************
